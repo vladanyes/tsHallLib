87e135c27749f45affd6291b3bad19f32aa529e3
Vlad Ryabinin <vlad.ryabinin@softomate.com>
2018-10-16 12:56:52 +0700

    added rollup config to commit

--- rollup.config.ts
+++ rollup.config.ts
@@ -7,7 +7,7 @@ import json from 'rollup-plugin-json'
 
 const pkg = require('./package.json')
 
-const libraryName = 'tslib'
+const libraryName = 'tsHallLib'
 
 export default {
   input: `src/${libraryName}.ts`,
