06cfd8d6bf1988aa80614cc00385755ceed6a8b3
Vlad Ryabinin <vlad.ryabinin@softomate.com>
2018-10-16 12:22:09 +0700

    added hall interface to the lib

--- src/tslib.ts
+++ src/tslib.ts
@@ -1,6 +1,30 @@
-// Import here Polyfills if needed. Recommended core-js (npm i -D core-js)
-  // import "core-js/fn/array.find"
-  // ...
-export default class DummyClass {
-
+export interface ResourceCollection<T, K extends string> {
+  _embedded: { [P in K]: Array<Resource<T>> }
+  _links: {
+    self: HalLink
+    next?: HalLink
+    prev?: HalLink
+  }
+  page: HalPage
 }
+
+export interface HalLink {
+  href: string
+  templated?: boolean
+}
+
+export interface HalPage {
+  size: number
+  totalElements: number
+  totalPages: number
+  number: number
+}
+
+export interface HalLinks {
+  _links: {
+    self: HalLink
+    landingPage: HalLink
+  }
+}
+
+export type Resource<T> = T & HalLinks
--- test/tslib.test.ts
+++ test/tslib.test.ts
@@ -1,14 +1,5 @@
-import DummyClass from "../src/tslib"
-
-/**
- * Dummy test
- */
-describe("Dummy test", () => {
-  it("works if true is truthy", () => {
+describe('Dummy test', () => {
+  it('works if true is truthy', () => {
     expect(true).toBeTruthy()
-  })
-
-  it("DummyClass is instantiable", () => {
-    expect(new DummyClass()).toBeInstanceOf(DummyClass)
   })
 })
